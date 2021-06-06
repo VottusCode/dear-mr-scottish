91d93e74ad8aeffeb623bd64bcca85837016a4a1
Mia <vottus@vott.us>
2021-06-06 23:15:31 +0200

    feat: init

---
+++ src/main.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./App.css";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <div className="w-screen h-screen bg-black text-white flex items-center">
+      <div className="max-w-6xl w-full mx-auto">
+        <h1 className="font-bold text-5xl">dear mr scottish...</h1>
+        <div className="text-2xl text-gray-200">
+          <p>
+            <p className="my-4">
+              from the bottom of my heart, i am deeply and truly sorry for
+              muting you and then proceeding to talk about you.
+            </p>
+            <p className="my-4">
+              This was obviously a big mistake, and i shall never do it again.
+              As i have deeply sinned to you our lord and saviour, i am writing
+              this heartfelt deep apology, which is taking up 5 hours. I am
+              writing this from the bottom of my deep big heart, and hopefully
+              you will find some light in me to accept me into your forgiving
+              and amazing arms, Mr.Scottish. I will never ever do this again, as
+              I know the consequenses, and i will be more careful with how my
+              words slip out of my shameless disgusting potty mouth.
+            </p>
+            <p className="my-4">Its a leo thing</p>
+            <p className="mt-6 font-bold text-pink-400">
+              Signed Jennifer {"<3"}
+            </p>
+          </p>
+        </div>
+      </div>
+    </div>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
